4322e2ea53b286adb4d18ab108f45952391d7fbd
Owen Conti <owen@ohseemedia.com>
2019-11-24 18:34:41 -0700

    Add variants for primary/secondary.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const Color = require('color');
 const { colors } = require('tailwindcss/defaultTheme');
+
+const primmary = '#2A415E';
+const secondary = '#CC3A5A';
 
 module.exports = {
  theme: {
@@ -6,10 +10,15 @@ module.exports = {
    black: colors.black,
    white: colors.white,
    gray: colors.gray,
-   primary: '#2A415E',
-   secondary: '#CC3A5A'
+   dark: '#0B121B',
+   light: '#F8FAFC',
+   primary: primary,
+   'primary-lighten': Color(primray).lighten(15),
+   'primary-darken': Color(primray).darken(15),
+   secondary: secondary,
+   'secondary-lighten': Color(secondary).lighten(15),
+   'secondary-darken': Color(secondary).darken(15)
   }
  },
- variants: {},
  plugins: [require('@tailwindcss/custom-forms')]
 };
