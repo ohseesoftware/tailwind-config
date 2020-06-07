aa36ba7014bc06ec23a79eb8e17c58c07e69abd7
Owen Conti <owen@ohseemedia.com>
2020-06-07 09:54:07 -0600

    feat: Update release process to publish to npm.

---
+++ release.config.js
@@ -0,0 +1,12 @@
+{
+  "plugins": [
+    "@semantic-release/npm",
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    ["@semantic-release/git", {
+      "assets": ["package.json"],
+      "message": "chore(release): ${nextRelease.version}"
+    }]
+  ]
+}
