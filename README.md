b0b39fb4fdc18eccabc47494c894202438139ec4
Owen Conti <owen@ohseemedia.com>
2020-06-07 09:23:28 -0600

    Add more brand colors. Update release process.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Oh See Software Tailwind Configuration
+
+The standard TailwindCSS configuration for a Oh See Software project.
+
+# Updating
+
+Committing updates will run semantic release as part of the GitHub Actions script.
--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const Color = require('color');
-const { colors } = require('tailwindcss/defaultTheme');
+const Color = require("color");
+const { colors } = require("tailwindcss/defaultTheme");
 
-const primary = '#2A415E';
-const secondary = '#CC3A5A';
+const brandColors = {
+  primary: "#2A415E",
+  secondary: "#CC3A5A",
+  red: "#F62727",
+  blue: "#3C8AFF",
+  green: "#00CA75",
+  orange: "#FF782D",
+  indigo: "#AC6BFF",
+};
 
 module.exports = {
- theme: {
-  colors: {
-   black: colors.black,
-   white: colors.white,
-   gray: colors.gray,
-   blue: colors.blue,
-   red: colors.red,
-   dark: '#0B121B',
-   light: '#F8FAFC',
-   primary: primary,
-   'primary-lighten': Color(primary)
-    .lighten(0.15)
-    .hex(),
-   'primary-darken': Color(primary)
-    .darken(0.15)
-    .hex(),
-   secondary: secondary,
-   'secondary-lighten': Color(secondary)
-    .lighten(0.15)
-    .hex(),
-   'secondary-darken': Color(secondary)
-    .darken(0.15)
-    .hex()
-  }
- }
+  theme: {
+    colors: Object.keys(brandColors).reduce(
+      (carry, color) => {
+        const hex = brandColors[color];
+
+        carry[color] = hex;
+        carry[`${color}-lighten`] = Color(hex).lighten(0.15).hex();
+        carry[`${color}-darken`] = Color(hex).darken(0.15).hex();
+
+        return carry;
+      },
+      {
+        gray: colors.gray,
+        black: "#000000",
+        white: "#FFFFFF",
+        dark: "#273240",
+        light: "#FFFFFF",
+      }
+    ),
+  },
 };
