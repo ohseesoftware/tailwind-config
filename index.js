b3885118d6b6d46b54b57034a558a5f2d8008905
Owen Conti <owen@ohseemedia.com>
2020-06-20 21:51:47 -0600

    BREAKING CHANGE: Prefix brand colors with `brand-`

--- index.js
+++ index.js
@@ -15,21 +15,14 @@ const brandColors = {
 
 module.exports = {
   theme: {
-    colors: Object.keys(brandColors).reduce(
-      (carry, color) => {
-        const hex = brandColors[color];
+    colors: Object.keys(brandColors).reduce((carry, color) => {
+      const hex = brandColors[color];
 
-        carry[color] = hex;
-        carry[`${color}-lighten`] = Color(hex).lighten(0.15).hex();
-        carry[`${color}-darken`] = Color(hex).darken(0.15).hex();
+      carry[`brand-${color}`] = hex;
+      carry[`brand-${color}-lighten`] = Color(hex).lighten(0.15).hex();
+      carry[`brand-${color}-darken`] = Color(hex).darken(0.15).hex();
 
-        return carry;
-      },
-      {
-        gray: colors.gray,
-        black: "#000000",
-        white: "#FFFFFF",
-      }
-    ),
+      return carry;
+    }, {}),
   },
 };
